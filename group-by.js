8f03d7d88745ab977f7ae2233d9b9ddbbdc24299
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-06-12 09:50:10 -0600

    chore: dynamic key example

--- group-by.js
+++ group-by.js
@@ -1,16 +1,20 @@
 let people = [
-    { name: 'Alice', age: 21, specializations: 'frontend', active: false },
-    { name: 'Max', age: 20, specializations: 'backend', active: true },
-    { name: 'Helen', age: 23, specializations: 'frontend', active: true },
-    { name: 'John', age: 22, specializations: 'backend', active: false },
-    { name: 'Andrew', age: 24, specializations: 'frontend', active: true },
-    { name: 'Steve', age: 19, specializations: 'backend', active: true },
-    { name: 'Kate', age: 25, specializations: 'frontend', active: false },
-    { name: 'Sam', age: 26, specializations: 'backend', active: true },
-    { name: 'Tom', age: 27, specializations: 'frontend', active: false },
-    { name: 'Nick', age: 28, specializations: 'backend', active: true }
+    {name: 'Alice', age: 21, specializations: 'frontend', active: false},
+    {name: 'Max', age: 20, specializations: 'backend', active: true},
+    {name: 'Helen', age: 23, specializations: 'frontend', active: true},
+    {name: 'John', age: 22, specializations: 'backend', active: false},
+    {name: 'Andrew', age: 24, specializations: 'frontend', active: true},
+    {name: 'Steve', age: 19, specializations: 'backend', active: true},
+    {name: 'Kate', age: 25, specializations: 'frontend', active: false},
+    {name: 'Sam', age: 26, specializations: 'backend', active: true},
+    {name: 'Tom', age: 27, specializations: 'frontend', active: false},
+    {name: 'Nick', age: 28, specializations: 'backend', active: true}
 ]
 
 const groupedPeople = Object.groupBy(people, 'specializations');
 
 console.log(groupedPeople);
+
+const groupedPeopleWithDynamicKey = Object.groupBy(people, person => `Active: ${person.active}`);
+
+console.log(groupedPeopleWithDynamicKey);
