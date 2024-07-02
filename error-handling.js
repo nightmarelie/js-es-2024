b4c528ed0db59dccf735d401c49abc6b72815978
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-07-02 09:50:10 -0600

    chore: rethrowing an error

--- error-handling.js
+++ error-handling.js
@@ -1,7 +1,11 @@
 const sendLog = (...args) => console.log(...args);
 
 async function asyncTask() {
-  await fetch('https://1jsonplaceholder.typicode.com/todos/1').catch(sendLog);
+  await fetch('https://1jsonplaceholder.typicode.com/todos/1').catch(err => {
+    sendLog('Error:', err);
+
+    throw new Error('Failed to fetch data', { cause: err });
+  });
 }
 
 await asyncTask();
