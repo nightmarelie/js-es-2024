4e6fa7c99ba1695905d217490856ac3c4bba858e
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-08-29 20:09:08 -0600

    chore: new promise

--- promise.js
+++ promise.js
@@ -1,4 +1,6 @@
-const testedPromise = new Promise((resolve, reject) => {
-    resolve('Promise is resolved');
+// const testedPromise = new Promise((resolve, reject) => {
+//     resolve('Promise is resolved');
+//
+// });
 
-});
+const { promise, reject, resolve } = Promise.withResolvers ();
