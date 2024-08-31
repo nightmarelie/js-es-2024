c10c95065b5cdf036d498e1b63625960cfb3d6bf
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-08-30 23:09:56 -0600

    chore: restore endpoint

--- error-handling.js
+++ error-handling.js
@@ -1,7 +1,7 @@
 const sendLog = (...args) => console.log(...args);
 
 async function asyncTask() {
-  await fetch('https://1jsonplaceholder.typicode.com/todos/1').catch(err => {
+  await fetch('https://jsonplaceholder.typicode.com/todos/1').catch(err => {
     sendLog('Error:', err);
 
     throw new Error('Failed to fetch data', { cause: err });
